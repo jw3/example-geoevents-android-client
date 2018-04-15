a33c1543dc19ef1653d0f8214c9d9fd6d18b0259
John Wass <jwass3@gmail.com>
2018-04-15 14:40:02 -0400

    display and track current position of the device

--- app/src/main/java/com/github/jw3/examplegeoeventsclient/MainActivity.kt
+++ app/src/main/java/com/github/jw3/examplegeoeventsclient/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.esri.arcgisruntime.mapping.ArcGISMap
 import com.esri.arcgisruntime.mapping.Basemap
+import com.esri.arcgisruntime.mapping.view.LocationDisplay
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +16,11 @@ class MainActivity : AppCompatActivity() {
         val map = ArcGISMap(Basemap.Type.TOPOGRAPHIC, 34.056295, -117.195800, 16)
 
         mapView.map = map
+
+        val ld = mapView.locationDisplay
+        ld.autoPanMode = LocationDisplay.AutoPanMode.NAVIGATION
+        if (!ld.isStarted)
+            ld.startAsync()
     }
 
     override fun onPause() {
