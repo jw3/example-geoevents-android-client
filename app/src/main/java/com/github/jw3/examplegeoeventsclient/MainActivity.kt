b4916f99e822f98bedb695b549a45581850c4781
John Wass <jwass3@gmail.com>
2018-04-15 11:22:10 -0400

    initial map view

--- app/src/main/java/com/github/jw3/examplegeoeventsclient/MainActivity.kt
+++ app/src/main/java/com/github/jw3/examplegeoeventsclient/MainActivity.kt
@@ -2,11 +2,28 @@ package com.github.jw3.examplegeoeventsclient
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.esri.arcgisruntime.mapping.ArcGISMap
+import com.esri.arcgisruntime.mapping.Basemap
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val map = ArcGISMap(Basemap.Type.TOPOGRAPHIC, 34.056295, -117.195800, 16)
+
+        mapView.map = map
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.pause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.resume()
     }
 }
