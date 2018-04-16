4c18b9df4b8115afd65ee9eb75eb6cc8eff051d3
John Wass <jwass3@gmail.com>
2018-04-15 21:07:38 -0400

    use imagery and print position and accuracy

--- app/src/main/java/com/github/jw3/geoevents/MainActivity.kt
+++ app/src/main/java/com/github/jw3/geoevents/MainActivity.kt
@@ -2,6 +2,7 @@ package com.github.jw3.geoevents
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.esri.arcgisruntime.mapping.ArcGISMap
 import com.esri.arcgisruntime.mapping.Basemap
 import com.esri.arcgisruntime.mapping.view.LocationDisplay
@@ -13,11 +14,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val map = ArcGISMap(Basemap.Type.TOPOGRAPHIC, 34.056295, -117.195800, 16)
+        val map = ArcGISMap(Basemap.Type.IMAGERY, 34.056295, -117.195800, 16)
 
         mapView.map = map
 
         val ld = mapView.locationDisplay
+
+        ld.addLocationChangedListener { e ->
+            val acc = e.location.horizontalAccuracy
+            val lon = e.location.position.x
+            val lat = e.location.position.y
+            Toast.makeText(this@MainActivity, "$lon:$lat to $acc units", Toast.LENGTH_LONG).show()
+        }
+
         ld.autoPanMode = LocationDisplay.AutoPanMode.NAVIGATION
         if (!ld.isStarted)
             ld.startAsync()
