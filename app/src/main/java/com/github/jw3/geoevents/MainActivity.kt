d99deceea069b51027ef985c6b48812b119522df
John Wass <jwass3@gmail.com>
2018-04-28 23:12:32 -0400

    initial websocket connection

--- app/src/main/java/com/github/jw3/geoevents/MainActivity.kt
+++ app/src/main/java/com/github/jw3/geoevents/MainActivity.kt
@@ -3,12 +3,24 @@ package com.github.jw3.geoevents
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.esri.arcgisruntime.location.AndroidLocationDataSource
 import com.esri.arcgisruntime.mapping.ArcGISMap
 import com.esri.arcgisruntime.mapping.Basemap
 import com.esri.arcgisruntime.mapping.view.LocationDisplay
+import com.github.bobby.rxwsocket.RxWSEvent
+import com.github.bobby.rxwsocket.RxWSocket
+import io.reactivex.BackpressureStrategy
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
 
 class MainActivity : AppCompatActivity() {
+    val request = Request.Builder()
+            .get()
+            .url("ws://10.0.2.2:9000/api/watch/device")
+            .build()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +32,10 @@ class MainActivity : AppCompatActivity() {
 
         val ld = mapView.locationDisplay
 
+        val ds = AndroidLocationDataSource(applicationContext)
+
+
+
         ld.addLocationChangedListener { e ->
             val acc = e.location.horizontalAccuracy
             val lon = e.location.position.x
@@ -30,6 +46,19 @@ class MainActivity : AppCompatActivity() {
         ld.autoPanMode = LocationDisplay.AutoPanMode.NAVIGATION
         if (!ld.isStarted)
             ld.startAsync()
+
+        RxWSocket(OkHttpClient(), request)
+                .webSocketFlowable(BackpressureStrategy.BUFFER)
+                .subscribe {
+                    when (it) {
+                        is RxWSEvent.OpenEvent -> println("Opened Flowable")
+                        is RxWSEvent.MessageStringEvent -> println("Receive Message String: " + it.text)
+                        is RxWSEvent.MessageByteEvent -> println("Receive Message Byte: " + it.bytes)
+                        is RxWSEvent.ClosingEvent -> println("Closing")
+                        is RxWSEvent.FailureEvent -> println("Failure")
+                        is RxWSEvent.ClosedEvent -> println("Closed")
+                    }
+                }
     }
 
     override fun onPause() {
